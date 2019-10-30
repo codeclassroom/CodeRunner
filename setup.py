632cc753cfb5c215143a6b40cdc9396ce1a88043
Bhupesh-V <varshneybhupesh@gmail.com>
2019-10-30 20:17:30 +0530

    ready for prod v0.2

--- setup.py
+++ setup.py
@@ -1,26 +1,23 @@
-from distutils.core import setup
-setup(
-  name = 'coderunner',         # How you named your package folder (MyLib)
-  packages = ['coderunner'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Judge0 API Interface written in Python',   # Give a short description about your library
-  author = 'Bhupesh Varshey',                   # Type in your name
-  author_email = 'varsheybhupesh@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/codeclassroom/CodeRunner',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/codeclassroom/CodeRunner/archive/v0.1.tar.gz',    # I explain this later on
-  keywords = ['Judge0 API', 'code runner', 'codeclassroom'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'requests',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-  ],
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="coderunner",
+    version="0.2",
+    author="Bhupesh Varshey",
+    author_email="varsheybhupesh@gmail.com",
+    description="Judge0 API Interface written in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/codeclassroom/CodeRunner",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        'Topic :: Software Development :: Build Tools',
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
